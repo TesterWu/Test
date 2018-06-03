06ad22bd5bf437a124feb3e23cc74bae7525b2ab
brand wu <brandwu@branddeMacBook-Pro.local>
2018-06-03 17:56:24 +0800

    test

---
+++ src/ApiTest/Controller.java
@@ -0,0 +1,9 @@
+package ApiTest;
+
+public class Controller {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World!!!");
+	}
+
+}
---
+++ src/ApiTest/OnService.java
@@ -0,0 +1,5 @@
+package ApiTest;
+
+public class OnService {
+
+}
